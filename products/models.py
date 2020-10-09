2df27ce73082779ec3840f226ede0ede8b2e8b0d
vishkid84 <vishalittr@gmail.com>
2020-10-09 23:14:59 +0000

    Added list to admin and sort by price in admin

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=250)
     friendly_name = models.CharField(max_length=250, null=True, blank=True)
 
@@ -13,6 +17,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=250)
     description = models.TextField()
