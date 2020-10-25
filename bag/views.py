7365617ce74c4da37e58031a9e5bd2c0a2524497
vishkid84 <vishalittr@gmail.com>
2020-10-25 23:05:28 +0000

    Updated messages in bag views

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -8,14 +8,13 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     current_bag = int(request.POST.get('current_bag'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     bag[item_id] = current_bag
     messages.success(request, f'Added {product.name} to your bag')
-    
     request.session['bag'] = bag
 
     return redirect(redirect_url)
@@ -23,9 +22,12 @@ def add_to_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     try:
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
-        return HttpResponse(staus=500)
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
