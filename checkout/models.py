72c91d5af432f74d5b7ab81c60219710e420e1e3
vishkid84 <vishalittr@gmail.com>
2020-10-30 22:56:34 +0000

    Added profile app, views, models and template

--- checkout/models.py
+++ checkout/models.py
@@ -4,11 +4,14 @@ from django.db.models import Sum
 from django.conf import Settings
 from django_countries.fields import CountryField
 from products.models import Product
+from profiles.models import UserProfile
 
 # Create your models here.
 class Order(models.Model):
 
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     name = models.CharField(max_length=200, null=False, blank=False)
     email = models.CharField(max_length=200, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
