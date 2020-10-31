c2c9497c197e910e37d4172bbd1f3f9883b17ad0
vishkid84 <vishalittr@gmail.com>
2020-10-31 22:39:52 +0000

    Added profile views, templated and related css and js

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render,get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
+from .forms import UserProfileForm
 
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+
+    orders = profile.orders.all()
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
