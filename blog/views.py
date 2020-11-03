a84c3c9d5dcd86205686d6e2aef6dba1484826de
vishkid84 <vishalittr@gmail.com>
2020-11-03 22:37:14 +0000

    Added pagination to the blog page

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
 from .models import Blog
 
 # Create your views here.
 def blogs(request):
     blog_list = Blog.objects.all()
     latest = Blog.objects.order_by('-date')[0:3]
+
+    paginator =Paginator(blog_list, 1)
+    page = request.GET.get('page', 1)
+
+    try:
+        number_of_blogs = paginator.page(page)
+    except PageNotAnInteger:
+        number_of_blogs = paginator.page(1)
+    except EmptyPage:
+        number_of_blogs = paginator.page(paginator.num_pages)
+
     template = 'blog/blogs.html'
     context = {
-        'blog_list': blog_list,
+        'number_of_blogs': number_of_blogs,
         'latest': latest,
+        'blog_list': number_of_blogs,
     }
     return render(request, template, context)
