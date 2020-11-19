376a0f43ba1ab102387dadcdb14993f95a695fc0
vishkid84 <vishalittr@gmail.com>
2020-11-19 22:14:36 +0000

    added database for heroku in settings

--- vdesign/settings.py
+++ vdesign/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = ')^h@)vij70n61%*2^s-5=r)(jbb16^az0di8e-(dts-i$7+en1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vdesign.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,12 +125,19 @@ WSGI_APPLICATION = 'vdesign.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
 
 
 # Password validation
